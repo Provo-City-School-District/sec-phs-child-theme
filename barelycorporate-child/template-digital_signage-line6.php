e8d39ffd11ce2f6c4f39f33b810b34762d426224
Espi <joshespi@gmail.com>
2022-02-18 12:37:00 -0700

    updated digital signage to be more responsive.

--- barelycorporate-child/template-digital_signage-line6.php
+++ barelycorporate-child/template-digital_signage-line6.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 	/* Template Name: Digital Signage Line 6 */
 ?>
 <head>
@@ -12,6 +12,23 @@
 	<style>
 		iframe, video, img {
 			overflow: hidden !important;
+		}
+		.container {
+		  position: relative;
+		  overflow: hidden;
+		  width: 100%;
+		  padding-top: 56.25%; /* 16:9 Aspect Ratio (divide 9 by 16 = 0.5625) */
+		}
+
+		/* Then style the iframe to fit in the container div with full height and width */
+		.responsive-iframe {
+		  position: absolute;
+		  top: 0;
+		  left: 0;
+		  bottom: 0;
+		  right: 0;
+		  width: 100%;
+		  height: 100%;
 		}
 	</style>
 </head>
@@ -29,17 +46,17 @@
 	) );
 	if($the_query->have_posts()) :
 		while ($the_query->have_posts()) : $the_query->the_post();
-			
+
 			if(get_field('google_media')) {
 				$url = get_field('google_media');
 				$url = str_replace('/pub?', '/embed?', $url)
 				?>
-					<iframe src="<?php echo $url ?>" frameborder="0" width="3820" height="2140" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay"></iframe>		
-				<?php	
+					<iframe src="<?php echo $url ?>" class="responsive-iframe"></iframe>
+				<?php
 			} elseif (get_field('video')){
 				?>
 				<video src="<?php the_field('video'); ?>" width="3820" height="2140" autoplay loop></video>
-				<?php		
+				<?php
 			} else {
 				?>
 				<img src="<?php the_field('image'); ?>" width="3820" height="2140" alt="" />
